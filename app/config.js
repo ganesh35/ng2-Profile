fe49a86306c873413b44e9fbc66f0c02f07ae07a
Ganesh <ganesh35@gmail.com>
2016-09-08 12:56:15 +0200

    1.0.0-beta.03

---
+++ app/config.js
@@ -0,0 +1,7 @@
+//
+// ===== File globals.ts    
+//
+'use strict';
+exports.sep = '/';
+exports.version = "22.2.2";
+//# sourceMappingURL=config.js.map
---
+++ app/globals.js
@@ -0,0 +1,8 @@
+//
+// ===== File globals.ts    
+//
+'use strict';
+exports.sep = '/';
+exports.version = "1.0.0-beta.03";
+exports.themeName = 'theme1';
+//# sourceMappingURL=globals.js.map
