e0aad1beb4ea5d78611ff250ce997b18e3cb8a5a
Ganesh <ganesh35@gmail.com>
2016-09-13 20:21:01 +0200

    Fixed bug with cookie concent

--- app/globals.ts
+++ app/globals.ts
@@ -2,11 +2,12 @@
 // ===== File globals.ts    
 //
 'use strict';
-
 export var sep='/';
 export var version: string="1.0.0-beta.03";   
 export var themeName='theme1';
+
 export var allowedPages = ['terms', 'privacy', 'cookies', "imprint"];
+
 export var cookieConcentEnabled = true;
 
 export var languageList = [
