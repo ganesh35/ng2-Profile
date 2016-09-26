0729e6db1b659a976b2b3371019393c0ee56f731
Ganesh <ganesh35@gmail.com>
2016-09-27 01:19:08 +0200

    theme3 included

--- app/globals.ts
+++ app/globals.ts
@@ -4,7 +4,7 @@
 'use strict';
 export var sep='/';
 export var version: string="1.0.0-beta.03";   
-export var themeName='theme1';
+export var themeName='theme3';
 
 export var allowedPages = ['terms', 'privacy', 'cookies', "imprint"];
 
